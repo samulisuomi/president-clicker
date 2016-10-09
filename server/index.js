44c6822b62e579d2761b9471c40e774dee9eb962
Samuli Suomi <smsuom@utu.fi>
2016-10-09 21:21:00 +0300

    Add file save logic

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const fs = require("fs");
+const io = require("socket.io")(http);
+const Big = require("big.js");
+const CronJob = require("cron").CronJob;
+const debugLog = require("debug-log")("debug");
+
+const fileName = "score.json";
+
 var trumpCount;
 var hillaryCount;
+
+app.disable("x-powered-by");
 
 app.get("/", function(req, res) {
     res.send("/");
@@ -11,21 +20,52 @@ app.get("/", function(req, res) {
 io.on("connection", function(socket) {
     socket.emit("score.trump", trumpCount)
     socket.emit("score.hillary", hillaryCount);
-    console.log("a user connected");
+    debugLog("a user connected");
     socket.on("trump", function(msg) {
-        trumpCount++;
+        trumpCount = trumpCount.plus(1);
         socket.broadcast.emit("score.trump", trumpCount)
     });
     socket.on("hillary", function(msg) {
-        hillaryCount++;
+        hillaryCount = hillaryCount.plus(1);
         socket.broadcast.emit("score.hillary", hillaryCount);
     });
 });
 
 http.listen(3000, function() {
-    trumpCount = 0;
-    hillaryCount = 0;
+    var backup = JSON.parse(fs.readFileSync(fileName, "utf8"));
+    trumpCount = new Big(backup.t);
+    hillaryCount = new Big(backup.h);
     console.log("listening on *:3000");
 });
 
-// Send score to everyone every n seconds:
+const exitHandler = function(options, err) {
+    console.log("exitHandler");
+    if (err) console.log("- " + err.stack);
+    fs.writeFileSync(fileName + ".exit", JSON.stringify({"t": trumpCount.toFixed(), "h": hillaryCount.toFixed()}), "utf8");
+    if (options.exit) process.exit();
+}
+
+const saveFile = function(fileName) {
+    debugLog("Backing up score...");
+    fs.writeFile(fileName, JSON.stringify({"t": trumpCount.toFixed(), "h": hillaryCount.toFixed()}), "utf8", function(err) {
+        if (err) {
+            console.log("ERROR: File backup failed.");
+        } else {
+            debugLog("Backup succeeded!");
+        }
+    });
+}
+
+//do something when app is closing
+process.on("exit", exitHandler.bind(null));
+
+//catches ctrl+c event
+process.on("SIGINT", exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on("uncaughtException", exitHandler.bind(null, {exit:true}));
+
+// Save score to file every 10 seconds:
+setInterval(function() {
+    saveFile(fileName);
+}, 10000);
